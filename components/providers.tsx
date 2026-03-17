87fadd09e484b3ed1841a2968eec45d3d728894f
ArjandH <arjandenh@outlook.com>
2026-03-17 11:33:02 +0100

    feat: wrap Providers component with SessionProvider for authentication context

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,13 +1,16 @@
 "use client";
 
+import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ToastProvider } from "@/components/providers/toast-provider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider>
-      {children}
-      <ToastProvider />
-    </ThemeProvider>
+    <SessionProvider>
+      <ThemeProvider>
+        {children}
+        <ToastProvider />
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
